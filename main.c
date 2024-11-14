fc24df3ea82f9fbf16d42cf8640039c82517d827
Vadim <kulikovskiiv@mail.ru>
2024-11-14 21:40:37 +0300

    3

--- main.c
+++ main.c
@@ -9,6 +9,43 @@ typedef struct node {
     struct node* next;
 } node;
 
+node** createAdj(int** G, int n) {
+    node** A = (node**)malloc(n * sizeof(node*));
+    for (int i = 0; i < n; ++i) {
+        A[i] = NULL;
+        node* tail = NULL;
+        for (int j = 0; j < n; ++j) {
+            if (G[i][j] == 1) {
+                node* newNode = (node*)malloc(sizeof(node));
+                newNode->n = j;
+                newNode->next = NULL;
+                if (tail == NULL) {
+                    A[i] = newNode;
+                }
+                else {
+                    tail->next = newNode;
+                }
+                tail = newNode;
+            }
+        }
+    }
+    return A;
+}
+
+void printAdj(node** A, int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("%d: ", i);
+        node* current = A[i];
+        while (current != NULL) {
+            printf("%d ", current->n);
+            current = current->next;
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
 int** createG(int n) {
     int** G = (int**)malloc(n * sizeof(int*));
     for (int i = 0; i < n; ++i) {
@@ -55,23 +92,51 @@ void check_vis(int** G, int n, int* vis) {
     }
 }
 
+void DFSA(node** A, int* vis, int s) {
+    vis[s] = 1;
+    printf("%d ", s);
+    node* current = A[s];
+    while (current != NULL) {
+        if (vis[current->n] == 0) {
+            DFSA(A, vis, current->n);
+        }
+        current = current->next;
+    }
+}
 
-
+void check_visA(node** A, int n, int* vis) {
+    for (int i = 0; i < n; ++i) {
+        if (vis[i] == 0) {
+            DFSA(A, vis, i);
+            printf("\n");
+        }
+    }
+}
 
 int main() {
     setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
     int n = 4;
+
     int* vis = (int*)malloc(sizeof(int) * n);
     for (int i = 0; i < n; ++i) vis[i] = 0;
-    int** M1 = NULL;
     printf("Введите количество вершин: ");
-    srand(time(NULL));
-
     //scanf("%d", &n);
     printf("\n");
 
-    M1 = createG(n);
+    int** M1 = createG(n);
     printf("Граф G1\n");
     printG(M1, n);
     check_vis(M1, n, vis);
+    printf("\n");
+
+    node** A1 = createAdj(M1, n);
+    printf("Список смежности графа G1\n");
+    printAdj(A1, n);
+    for (int i = 0; i < n; ++i) vis[i] = 0;
+    check_visA(A1, n, vis);
+
+    free(M1);
+    free(A1);
+    free(vis);
 }
